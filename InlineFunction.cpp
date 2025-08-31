7f4eee685a4a7ac71b8867578fbec392b1acf5b5
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-31 23:40:36 +0600

    declare inline.

---
+++ InlineFunction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+
+	// declare inline
+	inline int square(int x);
+};
+
+// Define the function
+// inline int A::square(int x) {
+// 	return x*x;
+// }
+
+int main() {
+	A obj;
+	cout << obj.square(3);
+	return 0;
+}
